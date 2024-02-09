3240a64775d9991c4b0a5ad08c182c3995629a5c
Billybobbe <73495053+Billybobbe@users.noreply.github.com>
2024-02-09 09:30:20 -0800

    Added functionality to images and buttons.

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import ImageViewer from './Components/ImageViewer';
+import Button from './Components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
+const PlaceholderImage = require('./assets/istockphoto-1388186992-612x612.jpg')
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+
+  const pickImageAsync = async() => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if(!result.canceled){
+      setSelectedImage(result.assets[0].uri)
+      setShowAppOptions(true);
+    }else{
+      alert('You did not select any image.');
+    }
+  };
+
   return (
+    
     <View style={styles.container}>
-      <Text style={{color:'#fff'}}>Goodbye expo!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage}/>
+      </View>
+      {showAppOptions?(
+        <View/>
+      ):(
+      <View style={styles.footerContainer}>
+        <Button onPress={pickImageAsync} theme="primary" label="Choose a photo"/>
+        <Button onPress={()=>setShowAppOptions(true)} label="Use this photo"/>
+       </View>
+      )}
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
   container: {
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  footerContainer:{
+    flex: 1/3,
+    alignItems: 'center',
   },
 });
